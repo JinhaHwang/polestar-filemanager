fb93b8d0b306d2abf813fabc154b0b68385663a0
황진하 <gojinha@naver.com>
2020-02-18 19:04:58 +0900

    storybook 설정 추가

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+    stories: ['../src/**/*.stories.js'],
+    addons: [
+        {
+            name: '@storybook/preset-create-react-app',
+            options: {
+                craOverrides: {
+                    fileLoaderExcludes: ['less'],
+                },
+            },
+        },
+        '@storybook/preset-ant-design',
+        '@storybook/addon-actions',
+        '@storybook/addon-links',
+        '@storybook/addon-storysource',
+        '@storybook/addon-knobs',
+    ],
+}
