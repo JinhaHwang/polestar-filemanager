3f21d820993460b7435d5bf109323e4bb600d52b
jinha <gojinha@naver.com>
2020-02-24 00:57:44 +0900

    prettier 설정 추가, proptypes 추가

--- src/components/DirectoryTree.js
+++ src/components/DirectoryTree.js
@@ -1,90 +1,57 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Tree } from 'antd'
 
 const { TreeNode, DirectoryTree: AntDirectoryTree } = Tree
 
-// class DirectoryTree extends React.Component {
-//     static propTypes = {
-//         items: PropTypes.array,
-//     }
-//     static defaultProps = {
-//         items: null,
-//     }
-//
-//     onSelect = (keys, event) => {
-//         console.log('Trigger Select', keys, event)
-//     }
-//
-//     onExpand = () => {
-//         console.log('Trigger Expand')
-//     }
-//
-//     renderTreeItems = (items) => {
-//         if (items === undefined || items === null) {
-//             return null;
-//         }
-//         if (Array.isArray(items)) {
-//             return items.map((item) => {
-//                 return this.renderTreeForHierarchy(item);
-//             });
-//         }
-//         return this.renderTreeForHierarchy(items);
-//     }
-//
-//     renderTreeForHierarchy = (item) => {
-//         const { idKey, childrenKey/* , ondemand */ } = this.props;
-//         if (item === null || Object.keys(item).length === 0) {
-//             return null;
-//         }
-//         // const isLeaf = ondemand ? item.isLeaf : undefined;
-//         // node icon
-//         const icon = this.renderIcon(item);
-//         let iconEl;
-//         if (icon) {
-//             iconEl = typeof icon === 'string' ? <Icon name={icon} /> : icon;
-//         } else if (item.icon) {
-//             iconEl = typeof item.icon === 'string' ? <Icon name={item.icon} /> : item.icon;
-//         }
-//
-//         // node label
-//         const labelEl = this.renderLabel(item);
-//         return (
-//             <TreeNode
-//                 key={item[idKey]}
-//                 // icon={(item.icon) ? <Icon name={item.icon} /> : null}
-//                 icon={iconEl}
-//                 // title={this.searhHighlight(item[labelKey])}
-//                 title={labelEl}
-//                 isLeaf={item.isLeaf}
-//                 dataRef={item}
-//                 disabled={item.disabled}
-//                 disableCheckbox={item.disableCheckbox}
-//             >
-//                 {(item[childrenKey] && item[childrenKey].length > 0) ? item[childrenKey].map((childItem) => {
-//                     return this.renderTreeForHierarchy(childItem);
-//                 }) : null}
-//             </TreeNode>
-//         );
-//     }
-//
-//     render() {
-//         const { items } = this.props
-//         return (
-//             <AntDirectoryTree
-//                 multiple
-//                 defaultExpandAll
-//                 onSelect={this.onSelect}
-//                 onExpand={this.onExpand}
-//                 expandAction="doubleClick"
-//             >
-//                 {this.renderTreeItems}
-//                 <TreeNode title="parent 0" key={`0-0`}>
-//                     <TreeNode title="leaf 0-0" key={`0-0-0`} isLeaf />
-//                     <TreeNode title="leaf 0-1" key={`0-0-1`} isLeaf />
-//                 </TreeNode>
-//             </AntDirectoryTree>
-//         )
-//     }
-// }
-//
-// export default DirectoryTree
+class DirectoryTree extends React.Component {
+    static propTypes = {
+      items: PropTypes.array
+    };
+
+    static defaultProps = {
+      items: [
+          { title: 'Expand to load', key: '0' },
+          { title: 'Expand to load', key: '1' },
+          { title: 'Tree Node', key: '2', isLeaf: true },
+      ]
+    };
+
+    onSelect = (keys, event) => {
+        console.log('Trigger Select', keys, event)
+    }
+
+    onExpand = () => {
+        console.log('Trigger Expand')
+    }
+
+    renderTreeNodes = data =>
+        data ? data.map(item => {
+            if (item.children) {
+                return (
+                    <TreeNode title={item.title} key={item.key} dataRef={item}>
+                        {this.renderTreeNodes(item.children)}
+                    </TreeNode>
+                )
+            }
+            return <TreeNode key={item.key} {...item} dataRef={item} />
+        }) : null
+
+    render() {
+        const { items, ...rest } = this.props
+        return (
+            <AntDirectoryTree
+                {...rest}
+                multiple
+                defaultExpandAll
+                onSelect={this.onSelect}
+                onExpand={this.onExpand}
+                expandAction="doubleClick"
+            >
+                {this.renderTreeNodes(items)}
+            </AntDirectoryTree>
+        )
+    }
+}
+
+export default DirectoryTree
