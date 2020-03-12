885a31299f3fa3426684c7c8bb4e18a39957c9a7
jinha <gojinha@naver.com>
2020-03-13 01:02:56 +0900

    FileGrid 스토리 개선 - 데코레이터를 이용한 프로바이더 제공 데이터를 전달 하는 부분은 추후 추가할 예정

--- src/components/molecules/FileGrid.stories.js
+++ src/components/molecules/FileGrid.stories.js
@@ -1,57 +1,55 @@
 // eslint-disable-next-line max-classes-per-file
-import React, {useCallback, useEffect, useRef} from 'react'
+import React from 'react'
 import { Provider } from 'react-redux'
 import base from 'paths.macro'
-import moment from 'moment'
 import FileGrid from 'components/molecules/FileGrid'
-import FileGridContainer from 'components/molecules/FileGridContainer'
-import {constApp, constFileGrid} from '../../common/constants'
 import configureStore from '../../redux/stores/configureStore'
-import App from "../.."
 
 export default {
     title: `${base}FileGrid`,
     component: FileGrid,
+    decorators: [
+        storyFn => {
+            const store = configureStore()
+            return <Provider store={store}>{storyFn()}</Provider>
+        },
+    ],
 }
 
 // HistoryNavigator 때문에 Provider를 통해 store 전달 필요
-export const index = () => <FileGrid />
-
-index.story = {
-    name: 'default',
-}
+export const defaultFileGrid = () => <FileGrid />
 
 export const overwriteHeight = () => <FileGrid height={200} />
 
-export const rowData = () => {
-    const addIsDirectoryKeyByMode = ({ mode, ...file }) => ({
-        ...file,
-        mode,
-        isDirectory: mode.charAt(0) === 'd',
-    })
-    const data = [
-        {
-            name: 'test',
-            mode: '-rw-r--r--',
-            owner: 'hwangjinha',
-            group: 'staff',
-            size: '75B',
-            modified: moment(),
-        },
-        {
-            name: 'test2',
-            mode: 'drwxr-xr-x',
-            owner: 'hwangjinha',
-            group: 'staff',
-            size: '75B',
-            modified: moment(),
-        },
-    ]
+// todo : 데이터 전달하는 스토리는 좀 고민해봐야할 듯
+// export const rowData = () => {
+//     const addIsDirectoryKeyByMode = ({ mode, ...file }) => ({
+//         ...file,
+//         mode,
+//         isDirectory: mode.charAt(0) === 'd',
+//     })
+//     const data = [
+//         {
+//             name: 'test',
+//             mode: '-rw-r--r--',
+//             owner: 'hwangjinha',
+//             group: 'staff',
+//             size: '75B',
+//             modified: moment(),
+//         },
+//         {
+//             name: 'test2',
+//             mode: 'drwxr-xr-x',
+//             owner: 'hwangjinha',
+//             group: 'staff',
+//             size: '75B',
+//             modified: moment(),
+//         },
+//     ]
+//
+//     return <FileGrid rowData={data.map(addIsDirectoryKeyByMode)} />
+// }
 
-    return <FileGrid rowData={data.map(addIsDirectoryKeyByMode)} />
-}
-
-// TODO events 와 data 스토리 작성
 export const events = () => {
     const handleGridReady = params => {
         console.log(params)
